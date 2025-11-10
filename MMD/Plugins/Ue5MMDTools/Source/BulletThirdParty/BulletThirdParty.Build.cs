faf342e1e818a17407ce14a868dc7933044346ba
treasureGrove <1324236706@qq.com>
2025-11-10 15:40:46 +0800

    Vendorize Bullet: build Bullet libs and link in module build script

--- MMD/Plugins/Ue5MMDTools/Source/BulletThirdParty/BulletThirdParty.Build.cs
+++ MMD/Plugins/Ue5MMDTools/Source/BulletThirdParty/BulletThirdParty.Build.cs
@@ -17,33 +17,41 @@ public class BulletThirdParty : ModuleRules
             PublicIncludePaths.Add(BulletInclude);
         }
 
-        // Link prebuilt libs if available under lib/<platform>
-        string LibDir = Path.Combine(BulletRoot, "lib", Target.Platform.ToString());
-        if (Directory.Exists(LibDir))
+        // Look for prebuilt libs in build/lib/Release or lib/<platform>
+        string BuildLibRelease = Path.Combine(BulletRoot, "build", "lib", "Release");
+        if (Directory.Exists(BuildLibRelease) && Target.Platform == UnrealTargetPlatform.Win64)
+        {
+            string dyn = Path.Combine(BuildLibRelease, "BulletDynamics.lib");
+            string col = Path.Combine(BuildLibRelease, "BulletCollision.lib");
+            string lin = Path.Combine(BuildLibRelease, "LinearMath.lib");
+            if (File.Exists(dyn)) PublicAdditionalLibraries.Add(dyn);
+            if (File.Exists(col)) PublicAdditionalLibraries.Add(col);
+            if (File.Exists(lin)) PublicAdditionalLibraries.Add(lin);
+        }
+
+        // Fallback search in lib/<platform>
+        string LibDirPlatform = Path.Combine(BulletRoot, "lib", Target.Platform.ToString());
+        if (Directory.Exists(LibDirPlatform))
         {
             if (Target.Platform == UnrealTargetPlatform.Win64)
             {
-                string dyn = Path.Combine(LibDir, "BulletDynamics.lib");
-                string col = Path.Combine(LibDir, "BulletCollision.lib");
-                string lin = Path.Combine(LibDir, "LinearMath.lib");
+                string dyn = Path.Combine(LibDirPlatform, "BulletDynamics.lib");
+                string col = Path.Combine(LibDirPlatform, "BulletCollision.lib");
+                string lin = Path.Combine(LibDirPlatform, "LinearMath.lib");
                 if (File.Exists(dyn)) PublicAdditionalLibraries.Add(dyn);
                 if (File.Exists(col)) PublicAdditionalLibraries.Add(col);
                 if (File.Exists(lin)) PublicAdditionalLibraries.Add(lin);
             }
             else
             {
-                // Unix-like static libs
-                string dyn = Path.Combine(LibDir, "libBulletDynamics.a");
-                string col = Path.Combine(LibDir, "libBulletCollision.a");
-                string lin = Path.Combine(LibDir, "libLinearMath.a");
+                string dyn = Path.Combine(LibDirPlatform, "libBulletDynamics.a");
+                string col = Path.Combine(LibDirPlatform, "libBulletCollision.a");
+                string lin = Path.Combine(LibDirPlatform, "libLinearMath.a");
                 if (File.Exists(dyn)) PublicAdditionalLibraries.Add(dyn);
                 if (File.Exists(col)) PublicAdditionalLibraries.Add(col);
                 if (File.Exists(lin)) PublicAdditionalLibraries.Add(lin);
             }
         }
-
-        // If no prebuilt libs found, recommend building Bullet with CMake into the lib folder.
-        // The build will still attempt to compile but may fail if headers reference missing symbols.
 
         // Expose include path to consumers
         PublicSystemIncludePaths.Add(BulletInclude);
